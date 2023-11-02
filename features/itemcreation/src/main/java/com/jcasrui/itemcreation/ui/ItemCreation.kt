b17a7362a48d002d992db72f0fddb07576fe79ba
jessicasrui <jcastroruiz23@gmail.com>
2023-11-02 21:43:39 +0100

    Interfaces Items
    Se han añadido las interfces itemCreation, itemDetail e itemList con sus string correspondientes.

---
+++ features/itemcreation/src/main/java/com/jcasrui/itemcreation/ui/ItemCreation.kt
@@ -0,0 +1,23 @@
+package com.jcasrui.itemcreation.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.moronlu18.itemcreation.R
+
+class ItemCreation : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item_creation, container, false)
+    }
+}
