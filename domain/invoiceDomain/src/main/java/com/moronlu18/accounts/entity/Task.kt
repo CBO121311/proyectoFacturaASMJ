e00d9b47d9d4be5271a03a649fb9ab848dbdbac1
Sergio García Vico <sergiogarciavicooficial@gmail.com>
2023-11-27 17:58:46 +0100

    Creacion de la carpeta Enum

--- domain/invoiceDomain/src/main/java/com/moronlu18/accounts/entity/Task.kt
+++ domain/invoiceDomain/src/main/java/com/moronlu18/accounts/entity/Task.kt
@@ -1,8 +1,14 @@
 package com.moronlu18.accounts.entity
 
+import com.moronlu18.accounts.enum.TaskStatus
+import com.moronlu18.accounts.enum.TypeTask
+
 data class Task(
+    val id: Int,
     val nomClient: String,
     val nomTask: String,
+    val typeTask: TypeTask,
+    val taskStatus: TaskStatus,
     val descTask: String,
     val fechCreation: String,
     val fechFinalization: String
---
+++ domain/invoiceDomain/src/main/java/com/moronlu18/accounts/enum/TypeTask.kt
@@ -0,0 +1,7 @@
+package com.moronlu18.accounts.enum
+enum class TypeTask {
+    TAREA_DOMÉSTICA,
+    TAREA_DE_ORGANIZACIÓN,
+    TAREA_DE_BRICOLAJE_Y_REPARACIONES,
+    TAREA_DE_APRENDIZAJE
+}
